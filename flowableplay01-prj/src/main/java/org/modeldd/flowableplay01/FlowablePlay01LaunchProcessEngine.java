1e4a6d693d5e74e03a9136a770017c2cfaae2c56
carrascoMDD <carrascomdd@gmail.com>
2018-05-25 23:17:58 +0200

    Create a flowable process engine and deploy BPMN2 specification

---
+++ flowableplay01-prj/src/main/java/org/modeldd/flowableplay01/FlowablePlay01LaunchProcessEngine.java
@@ -0,0 +1,55 @@
+/*
+ * org.modeldd.flowableplay01.FlowablePlay01.js
+ *
+ * Created @author Antonio Carrasco Valero 201805252222
+ *
+ *
+ ***************************************************************************
+
+ Copyright 2018 Antonio Carrasco Valero
+ Setup a Flowable ( https://github.com/flowable/flowable-engine) playgroung on top of Spring.
+ 
+Licensed under the EUPL, Version 1.1 only (the "Licence");
+You may not use this work except in compliance with the
+Licence.
+You may obtain a copy of the Licence at:
+https://joinup.ec.europa.eu/software/page/eupl/licence-eupl
+Unless required by applicable law or agreed to in
+writing, software distributed under the Licence is
+distributed on an "AS IS" basis,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+express or implied.
+See the Licence for the specific language governing
+permissions and limitations under the Licence.
+ {{License2}}
+
+ {{Licensed1}}
+ {{Licensed2}}
+
+ ***************************************************************************
+ *
+ */
+package org.modeldd.flowableplay01;
+
+import org.flowable.engine.ProcessEngine;
+import org.flowable.engine.ProcessEngineConfiguration;
+import org.flowable.engine.impl.cfg.StandaloneProcessEngineConfiguration;
+import org.flowable.engine.RepositoryService;
+import org.flowable.engine.repository.Deployment;
+import org.flowable.engine.repository.ProcessDefinition;
+
+public class FlowablePlay01LaunchProcessEngine {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		ProcessEngineConfiguration cfg = new StandaloneProcessEngineConfiguration()
+				.setJdbcUrl("jdbc:h2:mem:flowable;DB_CLOSE_DELAY=-1").setJdbcUsername("sa").setJdbcPassword("")
+				.setJdbcDriver("org.h2.Driver")
+				.setDatabaseSchemaUpdate(ProcessEngineConfiguration.DB_SCHEMA_UPDATE_TRUE);
+
+		ProcessEngine processEngine = cfg.buildProcessEngine();
+	}
+
+}
